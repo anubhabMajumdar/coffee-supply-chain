26f2b3b214854d9252a66f89898e75aad4f6336b
anubhabMajumdar <anubhabmajumdar93@gmail.com>
2019-10-14 00:43:45 -0700

    Adding js

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,11 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "https://rinkeby.infura.io/v3/";
+const metamaskKey = "";
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(metamaskKey, infuraKey),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000,
+    },
   },
+  
   compilers: {
      solc: {
        version: "0.4.24"  // ex:  "0.4.20". (Default: Truffle's installed solc)
